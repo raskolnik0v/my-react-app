130c822852439bb4325e8a3df4eb847960fd8c95
raskol <simbillicem@yandex.com>
2022-12-17 17:46:42 +0300

    react update

---
+++ src/özl.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class özl extends Component {
+  render() {
+    return (
+        <div>
+           React ın daha başındayız la 
+            <form>
+            <input type="text"/>
+            <button> Kaydet</button>
+            </form>
+        </div>
+
+    )
+  }
+}
